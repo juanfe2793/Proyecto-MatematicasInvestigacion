893a46a9e00affd1bda4526507d58f0fedd6d759
andres2508 <jaime.aristizabal.2508@gmail.com>
2018-04-27 15:23:01 -0500

    Trabajo
    Bitácora de Buzz Light Year, Intento numero 1, No No Noooooo

--- myfun.m
+++ myfun.m
@@ -1,5 +1,17 @@
-function myfun = myfun(f)
+function [f, g, H] = myfun(X, data)
 
-                               
+t = data(:,1);
+f = (X(1,3)*exp(X(1,1).*t))+(X(1,4)*exp(X(1,2).*t));
+
+J = [X(1,3).*t.*exp(X(1,1).*t), X(1,4).*t.*exp(X(1,2).*t), exp(X(1,1).*t), exp(X(1,2).*t)];
+J = -J;
+
+g = (J')*f;
+
+
+H = (J')*(J);
+
+figure(2)
+plot(f);
     
 end
