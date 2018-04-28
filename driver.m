a02beec03351485abef30cff20ce72786af2a9fc
Juan Felipe Gómez <juan.gomez24@correo.icesi.edu.co>
2018-04-28 02:54:13 -0500

    Jacobiana y Gradiente Bien
    Se realiza la configuración del algoritmo para calcular la jacobiana. Se
    realiza el calculo del gradiente. Se intenta realizar el calculo de la
    Hesiana, pero se tienen errores para el calculo del vector de residuos.

--- driver.m
+++ driver.m
@@ -1,6 +1,10 @@
-function [f,xr] = driver() 
+function [data,xr] = driver() 
   %Función que carga los datos de la función e invoca las funciones del proyecto.
+  % Proyecto desarrollado por: Jaime Aristizabal y Juan Felipe Gómez.
   
+  %---------------Limpiar todo-----------------------%
+  
+  clc, clear all, close all
   %---------------------- Carga Datos ---------------- %
   
   data = dlmread('data.csv',';');
@@ -8,6 +12,8 @@ function [f,xr] = driver()
   t = data(:,1);
   optimo = (4*exp(-4*t))-(4*exp(-5*t));
   %inicial = (exp(-t))-(exp(-2*t));
+ 
+ 
   figure(1)
   %plot(t, inicial)
   plot(t, optimo)
--- myfun.m
+++ myfun.m
@@ -1,14 +1,35 @@
 function [f, g, H] = myfun(X, data)
 
 t = data(:,1);
-f = (X(1,3)*exp(X(1,1).*t))+(X(1,4)*exp(X(1,2).*t));
+y = data(:,2);
+f = (X(1,3)*exp(X(1,1)*t))+(X(1,4)*exp(X(1,2)*t));
 
-J = [X(1,3).*t.*exp(X(1,1).*t), X(1,4).*t.*exp(X(1,2).*t), exp(X(1,1).*t), exp(X(1,2).*t)];
+%--------------- Calculo Jacobiano ------------------------%
+J = zeros(length(t), 4);    
+    for i = 1:length(t)
+        J(i,:) = [-X(1,3)*t(i)*exp(X(1,1)*t(i)) -X(1,4)*t(i)*exp(X(1,2)*t(i)) -exp(X(1,1)*t(i)) -exp(X(1,2)*t(i))];
+    end
+    
 J = -J;
 
-g = (J')*f;
+%-------------- Calculo Residuo -----------------------------%
 
+%m = zeros(length(t), 4)
 
+%for i = 1:lengt(t)
+    
+%    m(i,:) =(X(1,3)*exp(X(1,1)*t(i)))+(X(1,4)*exp(X(1,2)*t(i)))
+    
+%end
+m =  feval(f, X, t);
+
+r = y-m;
+
+%----------------- Calculo Gradiente ------------------------%
+
+g = (J')*r;
+
+%---------------- Calculo Hessiana --------------------------%
 H = (J')*(J);
 
 figure(2)
